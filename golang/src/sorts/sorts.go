7c73bfaaff744047349fa98fc00c338ff0d686c3
CloudLam <cloud.zero@qq.com>
2018-01-07 22:38:21 +0800

    go#2

--- golang/src/sorts/sorts.go
+++ golang/src/sorts/sorts.go
@@ -1,7 +1,9 @@
 // Copyright 2018 Cloud Lam. All rights reserved
+// Package sorts implements some sort methods
 
 package sorts
 
+// BubbleSort sorts array with Bubble Sort method
 func BubbleSort(array []int) {
 	var temp int
 	for i := 1; i < len(array); i++ {
@@ -10,6 +12,36 @@ func BubbleSort(array []int) {
 				temp = array[j]
 				array[j] = array[j+1]
 				array[j+1] = temp
+			}
+		}
+	}
+}
+
+// InsertSort sorts array with Insert Sort method
+func InsertSort(array []int) {
+	var temp int
+	for i := 1; i < len(array); i++ {
+		if array[i] < array[i-1] {
+			temp = array[i]
+			for j := i - 1; j >= 0 && array[j] > temp; j-- {
+				array[j+1] = array[j]
+				array[j] = temp
+			}
+		}
+	}
+}
+
+// ShellSort sorts array with Shell's Sort Method
+func ShellSort(array []int) {
+	var temp int
+	for gap := len(array) / 2; gap > 0; gap /= 2 {
+		for i := gap; i < len(array); i++ {
+			if array[i] < array[i-gap] {
+				for j := i - gap; j >= 0 && array[j] > array[j+gap]; j -= gap {
+					temp = array[j]
+					array[j] = array[j+gap]
+					array[j+gap] = temp
+				}
 			}
 		}
 	}
--- golang/src/test/test_sorts.go
+++ golang/src/test/test_sorts.go
@@ -4,12 +4,61 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
 	"sorts"
+	"time"
 )
 
+// SortTimes prints all sort method time cost
+func SortTimes() {
+	SortTime("Bubble")
+	SortTime("Insert")
+	SortTime("Shell")
+}
+
+// SortTime prints sort method time cost
+func SortTime(method string) {
+	// Build array
+	array := []int{}
+	for index := 0; index < 10240; index++ {
+		array = append(array, rand.Intn(100000))
+	}
+	// Begin time
+	begin := time.Now()
+	switch method {
+	case "Bubble":
+		sorts.BubbleSort(array)
+	case "Insert":
+		sorts.InsertSort(array)
+	case "Shell":
+		sorts.ShellSort(array)
+	default:
+	}
+	// Cost time
+	cost := time.Since(begin)
+	fmt.Println(method+" Sort Cost: ", cost)
+}
+
+// BubbleSortTest tests BubbleSort
 func BubbleSortTest() {
 	array := []int{89, 64, 18, 37, 95, 73, 25, 56, 43, 100}
-	fmt.Println(array)
+	fmt.Println("Source: ", array)
 	sorts.BubbleSort(array)
-	fmt.Println(array)
+	fmt.Println("Result: ", array)
+}
+
+// InsertSortTest tests InsertSort
+func InsertSortTest() {
+	array := []int{89, 64, 18, 37, 95, 73, 25, 56, 43, 100}
+	fmt.Println("Source: ", array)
+	sorts.InsertSort(array)
+	fmt.Println("Result: ", array)
+}
+
+// ShellSortTest tests ShellSort
+func ShellSortTest() {
+	array := []int{89, 64, 18, 37, 95, 73, 25, 56, 43, 100}
+	fmt.Println("Source: ", array)
+	sorts.ShellSort(array)
+	fmt.Println("Result: ", array)
 }

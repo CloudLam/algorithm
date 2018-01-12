697339be8a77d5133d9f899e4221f14b41f93fb5
CloudLam <cloud.zero@qq.com>
2018-01-12 23:00:18 +0800

    tree#0

---
+++ golang/src/tree/tree.go
@@ -0,0 +1,4 @@
+// Copyright 2018 Cloud Lam. All rights reserved
+// Package tree implements a binary tree
+
+package tree
---
+++ javascript/src/tree.js
@@ -0,0 +1,21 @@
+/*!
+ * tree.js v1.0.0
+ * 2017-2018 Cloud Lam
+ * Released under the MIT License.
+ */
+
+function Node (data, left, right) {
+  this.data = data || null;
+  this.left = left || null;
+  this.right = right || null;
+}
+
+class BinaryTree  {
+  constructor(node) {
+    this.node = node || null
+  }
+  insert () {}
+  preOrder () {}
+  inOrder () {}
+  postOrder () {}
+}

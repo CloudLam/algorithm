c8248ac08f9e900f6ebd57db1af50d11b427cbeb
CloudLam <cloud.zero@qq.com>
2018-01-13 22:53:15 +0800

    tree#1

--- javascript/src/tree.js
+++ javascript/src/tree.js
@@ -4,18 +4,92 @@
  * Released under the MIT License.
  */
 
-function Node (data, left, right) {
-  this.data = data || null;
-  this.left = left || null;
-  this.right = right || null;
+class Node {
+  constructor (value, left, right) {
+    this.value = value || null;
+    this.left = left || null;
+    this.right = right || null;
+  }
 }
 
-class BinaryTree  {
-  constructor(node) {
-    this.node = node || null
+class BinaryTree {
+  constructor (data) {
+    this.root = null;
+    if (data) {
+      data.forEach(value => {
+        this.insert(value)
+      });
+    }
   }
-  insert () {}
-  preOrder () {}
-  inOrder () {}
-  postOrder () {}
+  insert (value) {
+    let node = new Node(value);
+    if (this.root === null) {
+      this.root = node;
+    } else {
+      insertNode(this.root, node);
+    }
+    function insertNode(node, newNode) {
+      if (newNode.value < node.value) {
+        if (node.left === null) {
+          node.left = newNode;
+        } else {
+          insertNode(node.left, newNode);
+        }
+      } else {
+        if (node.right === null) {
+          node.right = newNode;
+        } else {
+          insertNode(node.right, newNode);
+        }
+      }
+    }
+  }
+  preOrder () {
+    let result = [];
+    preOrderTraversal(this.root);
+    return result;
+    function preOrderTraversal (node) {
+      if (node) {
+        result.push(node.value);
+      }
+      if (node.left) {
+        preOrderTraversal(node.left);
+      }
+      if (node.right) {
+        preOrderTraversal(node.right);
+      }
+    }
+  }
+  inOrder () {
+    let result = [];
+    inOrderTraversal(this.root);
+    return result;
+    function inOrderTraversal (node) {
+      if (node.left) {
+        inOrderTraversal(node.left);
+      }
+      if (node) {
+        result.push(node.value);
+      }
+      if (node.right) {
+        inOrderTraversal(node.right);
+      }
+    }
+  }
+  postOrder () {
+    let result = [];
+    inOrderTraversal(this.root);
+    return result;
+    function inOrderTraversal (node) {
+      if (node.left) {
+        inOrderTraversal(node.left);
+      }
+      if (node.right) {
+        inOrderTraversal(node.right);
+      }
+      if (node) {
+        result.push(node.value);
+      }
+    }
+  }
 }

4f67009d77821f4f9dee028439c6f327d601377c
CloudLam <cloud.zero@qq.com>
2018-01-23 23:58:31 +0800

    subseq#0

--- golang/src/subseqs/subseqs.go
+++ golang/src/subseqs/subseqs.go
@@ -1,11 +1,7 @@
 // Copyright 2018 Cloud Lam. All rights reserved
 // Package subseqs implements some subsequence methods
 
-package main
-
-import (
-	"fmt"
-)
+package subseqs
 
 // LCS returns Longest Common Subsequence
 func LCS(x, y string) (int, string) {
@@ -14,58 +10,41 @@ func LCS(x, y string) (int, string) {
 	}
 	lcs := ""
 	var dp [][]int
+	// Length of LCS
 	for i := 0; i <= len(x); i++ {
 		var temp []int
 		for j := 0; j <= len(y); j++ {
-			if i == 0 {
+			if i == 0 || j == 0 {
 				temp = append(temp, 0)
 				continue
 			}
 			if x[i-1] == y[j-1] {
 				temp = append(temp, dp[i-1][j-1]+1)
 			} else {
-				if dp[i-1][j] > dp[i][j-1] {
+				if dp[i-1][j] > temp[j-1] {
 					temp = append(temp, dp[i-1][j])
 				} else {
-					temp = append(temp, dp[i][j-1])
+					temp = append(temp, temp[j-1])
 				}
 			}
 		}
 		dp = append(dp, temp)
-		fmt.Println(dp)
+	}
+	// Subsequence of LCS
+	m := len(x)
+	n := len(y)
+	for m > 0 && n > 0 {
+		if x[m-1] == y[n-1] {
+			lcs = string(x[m-1]) + lcs
+			m--
+			n--
+			continue
+		}
+		if dp[m][n-1] > dp[m-1][n] {
+			n--
+		} else {
+			m--
+		}
 	}
 	return dp[len(x)][len(y)], lcs
 }
-
-func main() {
-	len, lcs := LCS("lcs", "lcs1")
-	fmt.Println(len, lcs)
-}
-
-/*var dp = [new Array(y.length + 1).fill(0)];
-  var lcs = '';
-  // Length of LCS
-  for (var i = 1; i <= x.length + 1; i++) {
-    dp[i] = [0];
-    for (var j = 1; j <= y.length; j++) {
-      if (x[i-1] == y[j-1]) {
-        dp[i][j] = dp[i-1][j-1] + 1;
-      } else {
-        dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
-      }
-    }
-  }
-  // Subsequence of LCS
-  for (var m = x.length, n = y.length; m > 0 && n > 0;) {
-    if (x[m-1] == y[n-1]) {
-      lcs = x[m-1] + lcs;
-      m--;
-      n--;
-      continue
-    }
-    if (dp[m][n-1] > dp[m-1][n]) {
-      n--;
-    } else {
-      m--;
-    }
-  }*/
---
+++ golang/src/test/test_subseqs.go
@@ -0,0 +1,13 @@
+// Copyright 2018 Cloud Lam. All rights reserved
+
+package main
+
+import (
+	"fmt"
+	"subseqs"
+)
+
+// LCSTest tests Longest Common Subsequence
+func LCSTest() {
+	fmt.Println(subseqs.LCS("lcs", "longest-common-subsequence"))
+}

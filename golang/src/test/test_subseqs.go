156280dbb28ec529c34cfea0b80b272ab2b14bb0
CloudLam <cloud.zero@qq.com>
2018-01-25 00:07:48 +0800

    subseq#0

--- golang/src/test/test_subseqs.go
+++ golang/src/test/test_subseqs.go
@@ -4,11 +4,25 @@ package main
 
 import (
 	"fmt"
+	"regexp"
 	"subseqs"
 )
 
 // LCSTest tests Longest Common Subsequence
 func LCSTest() {
-	len, lcs := subseqs.LCS("lcs", "longest-common-subsequence")
-	fmt.Println(len, lcs)
+	x := "lcs"
+	y := "longest-common-subsequence"
+
+	length, lcs := subseqs.LCS(x, y)
+
+	fmt.Println(length, lcs)
+
+	re := string(lcs[0])
+	for i := 1; i < len(lcs); i++ {
+		re += ".*" + string(lcs[i])
+	}
+	matchX, _ := regexp.Match(re, []byte(x))
+	matchY, _ := regexp.Match(re, []byte(y))
+
+	fmt.Println(re, matchX && matchY)
 }
